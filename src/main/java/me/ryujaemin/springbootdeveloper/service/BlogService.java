ac14f691ef2389c44d80766e10fa7827010035c1
류재민 <ryumin0421@gmail.com>
2025-05-19 21:01:25 +0900

    서비스 메서드 코드 작성하기

---
+++ src/main/java/me/ryujaemin/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,19 @@
+package me.ryujaemin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.ryujaemin.springbootdeveloper.domain.Article;
+import me.ryujaemin.springbootdeveloper.dto.AddArticleRequest;
+import me.ryujaemin.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
