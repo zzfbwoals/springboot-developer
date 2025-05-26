d486ae786479dc5ac4828ef49fa166bca95a522c
ryu jaemin <ryumin0421@gmail.com>
2025-05-26 13:57:32 +0900

    엔티티에 생성, 수정 시간 추가하기

--- src/main/java/me/ryujaemin/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/ryujaemin/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,8 +1,11 @@
 package me.ryujaemin.springbootdeveloper;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
