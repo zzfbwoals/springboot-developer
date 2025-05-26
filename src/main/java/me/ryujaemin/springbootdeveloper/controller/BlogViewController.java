69ac3578215bfe3f307d1c6e10f6c62f66e165c8
ryu jaemin <ryumin0421@gmail.com>
2025-05-26 13:51:23 +0900

    컨트롤러 메서드 작성하기

---
+++ src/main/java/me/ryujaemin/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,30 @@
+package me.ryujaemin.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.ryujaemin.springbootdeveloper.domain.Article;
+import me.ryujaemin.springbootdeveloper.dto.ArticleListViewResponse;
+
+import me.ryujaemin.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
