409a1b682a3e86e45d037c3124826d90a62bf165
류재민 <ryumin0421@gmail.com>
2025-05-26 20:37:36 +0900

    서비스 메서드 코드 작성하기

---
+++ src/main/java/me/ryujaemin/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.ryujaemin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.ryujaemin.springbootdeveloper.domain.User;
+import me.ryujaemin.springbootdeveloper.dto.AddUserRequest;
+import me.ryujaemin.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
